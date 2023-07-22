54e245cdfb9eb4781782eb115ea9b2054bf1edbb
themms <maximusnewera@mail.ru>
2023-07-23 02:23:23 +0600

    modified:   src/components/Modal.tsx

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 interface IModal {
   isVisible?: boolean;
   title: string;
-  content: number
+  content: string
   footer: string | JSX.Element;
   onClose: () => void;
   children?: React.ReactNode;
