4465ed541a618d8984899c9a3ba3cd46de3c034d
themms <maximusnewera@mail.ru>
2023-08-11 23:58:30 +0600

    modified:   src/constants/index.ts 	modified:   src/pages/AdminPanel.tsx

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,87 +6,106 @@ interface IHostel {
   busy: number,
   icon?: string;
   description: string,
-  deleted: boolean
+  deleted: boolean;
+  reserved: boolean;
 }
 export const rooms: IHostel[] = [
-  { 
-    name: "Зеленый лес", 
+  {
+    name: "Зеленый лес",
     places: 8,
     busy: 6,
     icon: hotel,
     description: "Отель расположен в центре города и предлагает номера с кондиционером и бесплатным Wi-Fi. В отеле есть ресторан, бар и терраса.",
-    deleted: false
+    deleted: false,
+    reserved: true
   },
-  { 
-    name: "Синий океан", 
+  {
+    name: "Синий океан",
     places: 12,
     busy: 6,
     icon: hotel,
     description: "Этот роскошный отель находится в самом сердце Берлина, в нескольких минутах ходьбы от знаменитого Бранденбургского ворота. В отеле есть рестораны, бары и спа-центр.",
-    deleted: false
+    deleted: false,
+    reserved: false
   },
-  { 
-    name: "Желтый дом", 
+  {
+    name: "Желтый дом",
     places: 6,
     busy: 6,
     icon: hotel,
     description: "Этот роскошный отель находится в парке размером 7 гектаров в центре города Баден-Баден. В отеле есть рестораны, бары и спа-центр.",
-    deleted: false
+    deleted: false,
+    reserved: false
   },
-  { 
-    name: "Красный камень", 
+  {
+    name: "Красный камень",
     places: 10,
     busy: 6,
     icon: hotel,
     description: "Этот роскошный отель находится в центре Мюнхена, всего в 5 минутах ходьбы от площади Мариенплац. В отеле есть рестораны, бары и спа-центр.",
-    deleted: false
+    deleted: false,
+    reserved: false
   },
-  { 
-    name: "Розовый цветок", 
+  {
+    name: "Розовый цветок",
     places: 4,
     busy: 6,
     icon: hotel,
     description: "Этот роскошный отель находится в центре Мюнхена, всего в 5 минутах ходьбы от площади Карлсплац. В отеле есть рестораны, бары и спа-центр.",
-    deleted: false
+    deleted: false,
+    reserved: false
   },
-  { 
-    name: "Оранжевый сок", 
+  {
+    name: "Оранжевый сок",
     places: 8,
     busy: 6,
     icon: hotel,
     description: "Этот роскошный отель находится в самом сердце Берлина, всего в нескольких минутах ходьбы от знаменитого улицы Курфюрстендамм. В отеле есть рестораны, бары и спа-центр.",
-    deleted: false
+    deleted: false,
+    reserved: false
   },
-  { 
-    name: "Фиолетовый дракон", 
+  {
+    name: "Фиолетовый дракон",
     places: 14,
     busy: 6,
     icon: hotel,
     description: "Этот роскошный отель находится в центре Мюнхена, всего в нескольких минутах ходьбы от площади Карлсплац. В отеле есть рестораны, бары и спа-центр.",
-    deleted: false
+    deleted: false,
+    reserved: false
   },
-  { 
-    name: "Белый снег", 
+  {
+    name: "Белый снег",
     places: 6,
     busy: 6,
     icon: hotel,
     description: "Этот роскошный отель находится в самом сердце Берлина, всего в нескольких минутах ходьбы от знаменитого улицы Фридрихштрассе. В отеле есть рестораны, бары и спа-центр.",
-    deleted: false
+    deleted: false,
+    reserved: false
   },
-  { 
-    name: "Черный кот", 
+  {
+    name: "Черный кот",
     places: 12,
     busy: 6,
     icon: hotel,
     description: "Этот роскошный отель находится в центре Мюнхена, всего в нескольких минутах ходьбы от площади Карлсплац. В отеле есть рестораны, бары и спа-центр.",
-    deleted: false
+    deleted: false,
+    reserved: false
   },
-  { 
-    name: "Коричневый медведь", 
+  {
+    name: "Коричневый медведь",
     places: 8,
     busy: 6,
     icon: hotel,
     description: "Этот роскошный отель находится в самом сердце Берлина, всего в нескольких минутах ходьбы от знаменитого Б",
-    deleted: false
+    deleted: false,
+    reserved: false
   }
-]
+];
+
+export const reserveItem = (index: number) => {
+  rooms[index].reserved = true;
+};
+
+export const unreserveItem = (index: number) => {
+  rooms[index].reserved = false;
+};
--- src/pages/AdminPanel.tsx
+++ src/pages/AdminPanel.tsx
@@ -1,5 +1,5 @@
 import { Modal } from "components/Modal";
-import { rooms } from "constants";
+import { reserveItem, rooms, unreserveItem } from "constants";
 import { useEffect, useState } from "react";
 
 interface IParseRooms {
@@ -8,13 +8,12 @@ interface IParseRooms {
   indexRoom: number,
   placesRoom: number,
   description: string,
+  reserved: boolean;
 }
 
 export const Admin = () => {
-  const [isLoading, setLoading] = useState(true);
   const [isModal, setIsModal] = useState(false);
   const [isSettingsModal, setSettingsModal] = useState(false);
-  const [reserve, setReserve] = useState(false);
 
   //string states
 
@@ -23,36 +22,12 @@ export const Admin = () => {
 
   //number states
 
-  const [indexRoom, setRoomIndex] = useState<number>();
+  const [indexRoom, setRoomIndex] = useState<number>(0);
   const [placesRoom, setPlacesRoom] = useState<number>(0);
   const [randomPlaces, setRandomPlaces] = useState(0);
-  const [imagesLoaded, setImagesLoaded] = useState(false);
 
-  useEffect(() => {
-    const imageElements = document.querySelectorAll('img');
-    const imageCount = imageElements.length;
-
-    let loadedCount = 0;
-
-    const handleImageLoad = () => {
-      loadedCount++;
-      if (loadedCount === imageCount) {
-        setImagesLoaded(true);
-      }
-    };
-
-    // Подписка на событие загрузки изображений
-    imageElements.forEach((img) => {
-      img.addEventListener('load', handleImageLoad);
-    });
-
-    // Отписка от событий загрузки изображений после размонтирования компонента
-    return () => {
-      imageElements.forEach((img) => {
-        img.removeEventListener('load', handleImageLoad);
-      });
-    };
-  }, []);
+  // reserve
+  const [reserve, setReserve] = useState<boolean>();
 
   function deleteRoom() {
     setIsModal(false);
@@ -63,14 +38,43 @@ export const Admin = () => {
 
   useEffect(() => {
     setRandomPlaces(Math.floor(Math.random() * 8 + 1));
-    console.log('generated');
   }, []);
 
-  /// Функция которая задает рандомное значение и возвращает элемент
+  const freeRoom = (index) => {
+    unreserveItem(index);
+    setReserve(false);
+  };
+
+  const reserveRoom = (index) => {
+    reserveItem(index);
+    setReserve(true);
+  };
+
+  const parseRoom = ({
+    name,
+    isModal,
+    indexRoom,
+    placesRoom,
+    description,
+    reserved,
+  }: IParseRooms) => {
+    // console.log(rooms[indexRoom]);
+    /// Имя комнаты
+    setRoomName(name);
+    /// Модальное окно активное или нет
+    setIsModal(isModal);
+    /// Индекс румы в объекте
+    setRoomIndex(indexRoom);
+    /// Места в руме
+    setPlacesRoom(placesRoom);
+    /// Описание румы в модалке
+    setDescriptionRoom(description);
+    /// Зарезервированна ли комната
+    setReserve(reserved);
+  };
+
+  /// Функция которая возвращает элемент
   function placesRooms(places: any) {
-    console.log(`PLACES / 2 =  ${places / 2}`);
-    console.log(`RANDOM PLACES ${randomPlaces}`);
-    () => setReserve(randomPlaces >= places / 2);
     return (
       <>
         <h1>{!reserve ? `Свободных мест ${randomPlaces} / ${places}` : `Отель забронирован ${randomPlaces} / ${places}`}</h1>
@@ -84,39 +88,10 @@ export const Admin = () => {
       </>
     );
   }
-  const parseRoom = ({
-    name,
-    isModal,
-    indexRoom,
-    placesRoom,
-    description
-  }: IParseRooms) => {
-    // console.log(rooms[indexRoom]);
-    /// Имя комнаты
-    setRoomName(name);
-    /// Модальное окно активное или нет
-    setIsModal(isModal);
-    /// Индекс румы в объекте
-    setRoomIndex(indexRoom);
-    /// Места в руме
-    setPlacesRoom(placesRoom);
-    /// Описание румы в модалке
-    setDescriptionRoom(description);
-  };
 
   return (
     <>
       <div className="grid grid-cols-1 rounded-lg">
-        {/* <ControlPanel
-          isModal={isModal}
-          setModal={setIsModal}
-          isSettingsModal={isSettingsModal}
-          setSettingsModal={setSettingsModal}
-          roomName={roomName}
-          indexRoom={indexRoom}
-          placesRoom={placesRooms(placesRoom)}
-          description={descriptionRoom}
-        /> */}
         <Modal
           isVisible={isModal}
           title={roomName}
@@ -167,13 +142,13 @@ export const Admin = () => {
               <button
                 className="w-1/2 text-gray-800 bg-green-500 rounded-md 
             outline-none border ring-offset-2"
-                onClick={() => setReserve(false)}>
+                onClick={() => freeRoom(indexRoom)}>
                 Отменить бронь
               </button>
               <button
                 className="w-1/2 text-gray-800 bg-red-500 rounded-md 
             outline-none border ring-offset-2"
-                onClick={() => setReserve(true)}>
+                onClick={() => { reserveRoom(indexRoom); console.log(rooms[indexRoom]); }}>
                 Забронировать
               </button>
             </>
@@ -182,7 +157,14 @@ export const Admin = () => {
         {rooms.map((room, index) => (
           <>
             <div className="hover:bg-gray-200 cursor-pointer pb-4" key={index} onClick={() => {
-              parseRoom({ name: room.name, isModal: true, placesRoom: room.places, description: room.description, indexRoom: index });
+              parseRoom({
+                name: room.name,
+                isModal: true,
+                placesRoom: room.places,
+                description: room.description,
+                indexRoom: index,
+                reserved: room.reserved
+              });
             }}>
               <h1 className="text-center mt-3">
                 {room.name}
